f088aec98198938add6cb859ba77e16902f57523
Archie Choa <archie.choa@gmail.com>
2018-11-07 05:58:22 +0800

    Update `Dockerfile`

--- crishante/settings.py
+++ crishante/settings.py
@@ -153,7 +153,7 @@ STATIC_URL = os.getenv('STATIC_URL', '/static/')
 # Media files
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.getenv('MEDIA_ROOT', os.path.join(BASE_DIR, 'media'))
 
 
 # Django Debug Toolbar
